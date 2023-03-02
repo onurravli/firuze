3652d3a86178a73eb5c7250f40ee890eaea1afd0
onurravli <onurravli@hotmail.com>
2023-03-02 11:49:50 +0300

    Update runner code

--- main.hs
+++ main.hs
@@ -3,6 +3,10 @@ import Tokenizer (parseTokens)
 main :: IO ()
 main = do
   let input = "abcdefg"
+  let input2 = "printf(\"%d\\n\", 23) + 5 / 2 &&"
   case parseTokens input of
+    Left err -> print err
+    Right tokens -> print tokens
+  case parseTokens input2 of
     Left err -> print err
     Right tokens -> print tokens
